48da0ddf8c5b926603a6b28aa0bf0bfcf3d885e9
Mikkel Meyer Andersen <mikl@mikl.dk>
2019-01-12 22:03:24 +0100

    work

--- tests/testthat/test-tcherry-all.R
+++ tests/testthat/test-tcherry-all.R
@@ -1,5 +1,7 @@
 context("Generate all third order t-cherry junction trees")
 
 #' 
-#' FIXME: test: all_tcherries(c("A", "B", "C", "D"), 2) === prufer(4)
+m <- all_tcherries(c("A", "B", "C", "D"), 2)
+m <- remove_equal_models(m)
+length(m) == lenght(prufer(4))
 
